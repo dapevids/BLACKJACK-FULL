9af014cee4b42f0caef00c6dcfe8da2a683313f7
David <david.m.polyak@gmail.com>
2025-12-06 13:53:04 -0500

    initial commit

---
+++ BLACKJACK FULL/Form1.Designer.vb
@@ -0,0 +1,395 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.start = New System.Windows.Forms.Button()
+        Me.hit = New System.Windows.Forms.Button()
+        Me.stand = New System.Windows.Forms.Button()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.p1 = New System.Windows.Forms.TextBox()
+        Me.p2 = New System.Windows.Forms.TextBox()
+        Me.p3 = New System.Windows.Forms.TextBox()
+        Me.p4 = New System.Windows.Forms.TextBox()
+        Me.p5 = New System.Windows.Forms.TextBox()
+        Me.p6 = New System.Windows.Forms.TextBox()
+        Me.d1 = New System.Windows.Forms.TextBox()
+        Me.d2 = New System.Windows.Forms.TextBox()
+        Me.d3 = New System.Windows.Forms.TextBox()
+        Me.d4 = New System.Windows.Forms.TextBox()
+        Me.d5 = New System.Windows.Forms.TextBox()
+        Me.d6 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.dTotal = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.pTotal = New System.Windows.Forms.TextBox()
+        Me.d6Card = New System.Windows.Forms.TextBox()
+        Me.d5Card = New System.Windows.Forms.TextBox()
+        Me.d4Card = New System.Windows.Forms.TextBox()
+        Me.d3Card = New System.Windows.Forms.TextBox()
+        Me.d2Card = New System.Windows.Forms.TextBox()
+        Me.d1Card = New System.Windows.Forms.TextBox()
+        Me.p6Card = New System.Windows.Forms.TextBox()
+        Me.p5Card = New System.Windows.Forms.TextBox()
+        Me.p4Card = New System.Windows.Forms.TextBox()
+        Me.p3Card = New System.Windows.Forms.TextBox()
+        Me.p2Card = New System.Windows.Forms.TextBox()
+        Me.p1Card = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'start
+        '
+        Me.start.Location = New System.Drawing.Point(85, 366)
+        Me.start.Name = "start"
+        Me.start.Size = New System.Drawing.Size(75, 23)
+        Me.start.TabIndex = 0
+        Me.start.Text = "start"
+        Me.start.UseVisualStyleBackColor = True
+        '
+        'hit
+        '
+        Me.hit.Location = New System.Drawing.Point(183, 366)
+        Me.hit.Name = "hit"
+        Me.hit.Size = New System.Drawing.Size(75, 23)
+        Me.hit.TabIndex = 1
+        Me.hit.Text = "hit"
+        Me.hit.UseVisualStyleBackColor = True
+        '
+        'stand
+        '
+        Me.stand.Location = New System.Drawing.Point(285, 366)
+        Me.stand.Name = "stand"
+        Me.stand.Size = New System.Drawing.Size(75, 23)
+        Me.stand.TabIndex = 2
+        Me.stand.Text = "stand"
+        Me.stand.UseVisualStyleBackColor = True
+        '
+        'reset
+        '
+        Me.reset.Location = New System.Drawing.Point(379, 365)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(75, 23)
+        Me.reset.TabIndex = 3
+        Me.reset.Text = "reset"
+        Me.reset.UseVisualStyleBackColor = True
+        '
+        'p1
+        '
+        Me.p1.Location = New System.Drawing.Point(36, 207)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(100, 22)
+        Me.p1.TabIndex = 4
+        '
+        'p2
+        '
+        Me.p2.Location = New System.Drawing.Point(158, 207)
+        Me.p2.Name = "p2"
+        Me.p2.Size = New System.Drawing.Size(100, 22)
+        Me.p2.TabIndex = 5
+        '
+        'p3
+        '
+        Me.p3.Location = New System.Drawing.Point(284, 207)
+        Me.p3.Name = "p3"
+        Me.p3.Size = New System.Drawing.Size(100, 22)
+        Me.p3.TabIndex = 6
+        '
+        'p4
+        '
+        Me.p4.Location = New System.Drawing.Point(407, 208)
+        Me.p4.Name = "p4"
+        Me.p4.Size = New System.Drawing.Size(100, 22)
+        Me.p4.TabIndex = 7
+        '
+        'p5
+        '
+        Me.p5.Location = New System.Drawing.Point(531, 207)
+        Me.p5.Name = "p5"
+        Me.p5.Size = New System.Drawing.Size(100, 22)
+        Me.p5.TabIndex = 8
+        '
+        'p6
+        '
+        Me.p6.Location = New System.Drawing.Point(657, 207)
+        Me.p6.Name = "p6"
+        Me.p6.Size = New System.Drawing.Size(100, 22)
+        Me.p6.TabIndex = 9
+        '
+        'd1
+        '
+        Me.d1.Location = New System.Drawing.Point(36, 58)
+        Me.d1.Name = "d1"
+        Me.d1.Size = New System.Drawing.Size(100, 22)
+        Me.d1.TabIndex = 10
+        '
+        'd2
+        '
+        Me.d2.Location = New System.Drawing.Point(158, 59)
+        Me.d2.Name = "d2"
+        Me.d2.Size = New System.Drawing.Size(100, 22)
+        Me.d2.TabIndex = 11
+        '
+        'd3
+        '
+        Me.d3.Location = New System.Drawing.Point(285, 59)
+        Me.d3.Name = "d3"
+        Me.d3.Size = New System.Drawing.Size(100, 22)
+        Me.d3.TabIndex = 12
+        '
+        'd4
+        '
+        Me.d4.Location = New System.Drawing.Point(407, 59)
+        Me.d4.Name = "d4"
+        Me.d4.Size = New System.Drawing.Size(100, 22)
+        Me.d4.TabIndex = 13
+        '
+        'd5
+        '
+        Me.d5.Location = New System.Drawing.Point(531, 59)
+        Me.d5.Name = "d5"
+        Me.d5.Size = New System.Drawing.Size(100, 22)
+        Me.d5.TabIndex = 14
+        '
+        'd6
+        '
+        Me.d6.Location = New System.Drawing.Point(657, 59)
+        Me.d6.Name = "d6"
+        Me.d6.Size = New System.Drawing.Size(100, 22)
+        Me.d6.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 20)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Dealer Hand"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(36, 173)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 16)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Player Hand"
+        '
+        'dTotal
+        '
+        Me.dTotal.Location = New System.Drawing.Point(158, 128)
+        Me.dTotal.Name = "dTotal"
+        Me.dTotal.Size = New System.Drawing.Size(100, 22)
+        Me.dTotal.TabIndex = 18
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(36, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 16)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "Dealer Total"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(36, 286)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 16)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Player Total"
+        '
+        'pTotal
+        '
+        Me.pTotal.Location = New System.Drawing.Point(158, 283)
+        Me.pTotal.Name = "pTotal"
+        Me.pTotal.Size = New System.Drawing.Size(100, 22)
+        Me.pTotal.TabIndex = 20
+        '
+        'd6Card
+        '
+        Me.d6Card.Location = New System.Drawing.Point(656, 96)
+        Me.d6Card.Name = "d6Card"
+        Me.d6Card.Size = New System.Drawing.Size(100, 22)
+        Me.d6Card.TabIndex = 27
+        '
+        'd5Card
+        '
+        Me.d5Card.Location = New System.Drawing.Point(530, 96)
+        Me.d5Card.Name = "d5Card"
+        Me.d5Card.Size = New System.Drawing.Size(100, 22)
+        Me.d5Card.TabIndex = 26
+        '
+        'd4Card
+        '
+        Me.d4Card.Location = New System.Drawing.Point(406, 96)
+        Me.d4Card.Name = "d4Card"
+        Me.d4Card.Size = New System.Drawing.Size(100, 22)
+        Me.d4Card.TabIndex = 25
+        '
+        'd3Card
+        '
+        Me.d3Card.Location = New System.Drawing.Point(284, 96)
+        Me.d3Card.Name = "d3Card"
+        Me.d3Card.Size = New System.Drawing.Size(100, 22)
+        Me.d3Card.TabIndex = 24
+        '
+        'd2Card
+        '
+        Me.d2Card.Location = New System.Drawing.Point(157, 96)
+        Me.d2Card.Name = "d2Card"
+        Me.d2Card.Size = New System.Drawing.Size(100, 22)
+        Me.d2Card.TabIndex = 23
+        '
+        'd1Card
+        '
+        Me.d1Card.Location = New System.Drawing.Point(35, 95)
+        Me.d1Card.Name = "d1Card"
+        Me.d1Card.Size = New System.Drawing.Size(100, 22)
+        Me.d1Card.TabIndex = 22
+        '
+        'p6Card
+        '
+        Me.p6Card.Location = New System.Drawing.Point(656, 245)
+        Me.p6Card.Name = "p6Card"
+        Me.p6Card.Size = New System.Drawing.Size(100, 22)
+        Me.p6Card.TabIndex = 33
+        '
+        'p5Card
+        '
+        Me.p5Card.Location = New System.Drawing.Point(530, 245)
+        Me.p5Card.Name = "p5Card"
+        Me.p5Card.Size = New System.Drawing.Size(100, 22)
+        Me.p5Card.TabIndex = 32
+        '
+        'p4Card
+        '
+        Me.p4Card.Location = New System.Drawing.Point(406, 245)
+        Me.p4Card.Name = "p4Card"
+        Me.p4Card.Size = New System.Drawing.Size(100, 22)
+        Me.p4Card.TabIndex = 31
+        '
+        'p3Card
+        '
+        Me.p3Card.Location = New System.Drawing.Point(284, 245)
+        Me.p3Card.Name = "p3Card"
+        Me.p3Card.Size = New System.Drawing.Size(100, 22)
+        Me.p3Card.TabIndex = 30
+        '
+        'p2Card
+        '
+        Me.p2Card.Location = New System.Drawing.Point(157, 245)
+        Me.p2Card.Name = "p2Card"
+        Me.p2Card.Size = New System.Drawing.Size(100, 22)
+        Me.p2Card.TabIndex = 29
+        '
+        'p1Card
+        '
+        Me.p1Card.Location = New System.Drawing.Point(35, 244)
+        Me.p1Card.Name = "p1Card"
+        Me.p1Card.Size = New System.Drawing.Size(100, 22)
+        Me.p1Card.TabIndex = 28
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(801, 452)
+        Me.Controls.Add(Me.p6Card)
+        Me.Controls.Add(Me.p5Card)
+        Me.Controls.Add(Me.p4Card)
+        Me.Controls.Add(Me.p3Card)
+        Me.Controls.Add(Me.p2Card)
+        Me.Controls.Add(Me.p1Card)
+        Me.Controls.Add(Me.d6Card)
+        Me.Controls.Add(Me.d5Card)
+        Me.Controls.Add(Me.d4Card)
+        Me.Controls.Add(Me.d3Card)
+        Me.Controls.Add(Me.d2Card)
+        Me.Controls.Add(Me.d1Card)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.pTotal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.dTotal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.d6)
+        Me.Controls.Add(Me.d5)
+        Me.Controls.Add(Me.d4)
+        Me.Controls.Add(Me.d3)
+        Me.Controls.Add(Me.d2)
+        Me.Controls.Add(Me.d1)
+        Me.Controls.Add(Me.p6)
+        Me.Controls.Add(Me.p5)
+        Me.Controls.Add(Me.p4)
+        Me.Controls.Add(Me.p3)
+        Me.Controls.Add(Me.p2)
+        Me.Controls.Add(Me.p1)
+        Me.Controls.Add(Me.reset)
+        Me.Controls.Add(Me.stand)
+        Me.Controls.Add(Me.hit)
+        Me.Controls.Add(Me.start)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents start As Button
+    Friend WithEvents hit As Button
+    Friend WithEvents stand As Button
+    Friend WithEvents reset As Button
+    Friend WithEvents p1 As TextBox
+    Friend WithEvents p2 As TextBox
+    Friend WithEvents p3 As TextBox
+    Friend WithEvents p4 As TextBox
+    Friend WithEvents p5 As TextBox
+    Friend WithEvents p6 As TextBox
+    Friend WithEvents d1 As TextBox
+    Friend WithEvents d2 As TextBox
+    Friend WithEvents d3 As TextBox
+    Friend WithEvents d4 As TextBox
+    Friend WithEvents d5 As TextBox
+    Friend WithEvents d6 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents dTotal As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents pTotal As TextBox
+    Friend WithEvents d6Card As TextBox
+    Friend WithEvents d5Card As TextBox
+    Friend WithEvents d4Card As TextBox
+    Friend WithEvents d3Card As TextBox
+    Friend WithEvents d2Card As TextBox
+    Friend WithEvents d1Card As TextBox
+    Friend WithEvents p6Card As TextBox
+    Friend WithEvents p5Card As TextBox
+    Friend WithEvents p4Card As TextBox
+    Friend WithEvents p3Card As TextBox
+    Friend WithEvents p2Card As TextBox
+    Friend WithEvents p1Card As TextBox
+End Class
---
+++ BLACKJACK FULL/Form1.vb
@@ -0,0 +1,373 @@
+﻿Imports System.Deployment.Application
+Imports System.Runtime.InteropServices.WindowsRuntime
+
+Public Class Form1
+    Public player1 As Double
+    Public player2 As Double
+    Public player3 As Double
+    Public player4 As Double
+    Public player5 As Double
+    Public player6 As Double
+
+    Public dealer1 As Double
+    Public dealer2 As Double
+    Public dealer3 As Double
+    Public dealer4 As Double
+    Public dealer5 As Double
+    Public dealer6 As Double
+
+    Public totalplayercard As Double
+    Public totaldealercard As Double
+
+    Public loopcheck As Double
+    Public startcheck As Double
+    Public hitcheck As Double
+    Public standcheck As Double
+    Public gameover As Double
+
+    Public deck(51) As String
+    Public ShuffledDeck(51) As String
+    Public ShuffledDeckValues(51) As Integer
+    Public Masterdeck() As String = {"2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "Jh", "Qh", "Kh", "Ah", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "Jd", "Qd", "Kd", "Ad", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "Jc", "Qc", "Kc", "Ac", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "Js", "Qs", "Ks", "As"}
+    Public PulledCrardIndex As Integer
+    Public CurrentCardValue As Integer
+    Public CurrentCardName As String
+    Public d2CardName As String
+
+
+    Private Sub start_Click(sender As Object, e As EventArgs) Handles start.Click
+        If startcheck = 1 Then
+            MessageBox.Show("you already started")
+        ElseIf startcheck = 0 Then
+            PulledCrardIndex = 0
+            CurrentCardValue = 0
+            CurrentCardName = ""
+            d2CardName = ""
+            create_a_deck()
+            shuffle_deck()
+
+            player1 = pullcard()
+            p1.Text = CurrentCardValue
+            p1Card.Text = CurrentCardName
+            player2 = pullcard()
+            p2.Text = CurrentCardValue
+            p2Card.Text = CurrentCardName
+
+            dealer1 = pullcard()
+            d1.Text = CurrentCardValue
+            d1Card.Text = CurrentCardName
+            dealer2 = pullcard()
+            d2.Text = "hidden"
+            d2CardName = CurrentCardName
+
+            startcheck = 1
+            hitcheck = 1
+            standcheck = 1
+            totalplayercard = player1 + player2
+
+            pTotal.Text = totalplayercard
+            dTotal.Text = ""
+
+            If totalplayercard = 21 Then
+                MessageBox.Show("youwin")
+                gameover = 1
+            End If
+        End If
+    End Sub
+
+    Private Sub hit_Click(sender As Object, e As EventArgs) Handles hit.Click
+        If gameover = 1 Then
+            MessageBox.Show("game is over hit reset")
+        ElseIf gameover = 0 Then
+            If hitcheck = 0 Then
+                MessageBox.Show("hit start")
+            ElseIf hitcheck = 4 Then
+                player6 = pullcard()
+                p6.Text = CurrentCardValue
+                p6Card.Text = CurrentCardName
+                hitcheck = 5
+            ElseIf hitcheck = 3 Then
+                player5 = pullcard()
+                p5.Text = CurrentCardValue
+                p5Card.Text = CurrentCardName
+                hitcheck = 4
+            ElseIf hitcheck = 2 Then
+                player4 = pullcard()
+                p4.Text = CurrentCardValue
+                p4Card.Text = CurrentCardName
+                hitcheck = 3
+            ElseIf hitcheck = 1 Then
+                player3 = pullcard()
+                p3.Text = CurrentCardValue
+                p3Card.Text = CurrentCardName
+                hitcheck = 2
+
+            End If
+
+            totalplayercard = player1 + player2 + player3 + player4 + player5 + player6
+            If totalplayercard > 21 Then
+                If player1 = 11 Then
+                    player1 = 1
+                    p1.Text = player1 & " was 11"
+                ElseIf player2 = 11 Then
+                    player2 = 1
+                    p2.Text = player2 & " was 11"
+                ElseIf player3 = 11 Then
+                    player3 = 1
+                    p3.Text = player3 & " was 11"
+                ElseIf player4 = 11 Then
+                    player4 = 1
+                    p4.Text = player4 & "was 11"
+                ElseIf player5 = 11 Then
+                    player5 = 1
+                    p5.Text = player5 & "was11"
+                ElseIf player6 = 11 Then
+                    player6 = 1
+                    p6.Text = player6 & "was 11 "
+
+                End If
+            End If
+            totalplayercard = player1 + player2 + player3 + player4 + player5 + player6
+            pTotal.Text = totalplayercard
+            If totalplayercard > 21 Then
+                MessageBox.Show("you lose")
+                gameover = 1
+            ElseIf totalplayercard = 21 Then
+                MessageBox.Show("you win")
+                gameover = 1
+            End If
+        End If
+    End Sub
+
+    Private Sub stand_Click(sender As Object, e As EventArgs) Handles stand.Click
+        If gameover = 1 Then
+            MessageBox.Show("hit reset first ")
+        ElseIf gameover = 0 Then
+            If standcheck = 0 Then
+                MessageBox.Show("hit start first")
+            ElseIf standcheck = 1 Then
+                d2.Text = dealer2
+                d2Card.Text = d2CardName
+                totaldealercard = dealer1 + dealer2
+                dTotal.Text = totaldealercard
+                If totaldealercard >= 17 Then
+                ElseIf totaldealercard < 17 Then
+                    Do While loopcheck = 0
+                        Select Case standcheck
+                            Case 4
+                                dealer6 = pullcard()
+                                d6.Text = CurrentCardValue
+                                d6Card.Text = CurrentCardName
+                                loopcheck = 1
+                            Case 3
+                                dealer5 = pullcard()
+                                d5.Text = CurrentCardValue
+                                d5Card.Text = CurrentCardName
+                                standcheck = 4
+                            Case 2
+                                dealer4 = pullcard()
+                                d4.Text = CurrentCardValue
+                                d4Card.Text = CurrentCardName
+                                standcheck = 3
+                            Case 1
+                                dealer3 = pullcard()
+                                d3.Text = CurrentCardValue
+                                d3Card.Text = CurrentCardName
+                                standcheck = 2
+                        End Select
+                        totaldealercard = dealer1 + dealer2 + dealer3 + dealer4 + dealer5 + dealer6
+                        If totaldealercard > 21 Then
+                            If dealer1 = 11 Then
+                                dealer1 = 1
+                                d1.Text = dealer1 & ("was11")
+                            ElseIf dealer2 = 11 Then
+                                dealer2 = 1
+                                d2.Text = dealer2 & ("was11")
+                            ElseIf dealer3 = 11 Then
+                                dealer3 = 1
+                                d3.Text = dealer3 & ("was 11")
+                            ElseIf dealer4 = 11 Then
+                                dealer4 = 1
+                                d4.Text = dealer4 & ("was11")
+                            ElseIf dealer5 = 11 Then
+                                dealer5 = 1
+                                d5.Text = dealer5 & ("was11")
+                            ElseIf dealer6 = 11 Then
+                                dealer6 = 1
+                                d6.Text = dealer6 & ("was 11")
+
+                            End If
+                        End If
+                        totaldealercard = dealer1 + dealer2 + dealer3 + dealer4 + dealer5 + dealer6
+                        dTotal.Text = totaldealercard
+                        If totaldealercard >= 17 Then
+                            loopcheck = 1
+
+                        End If
+                    Loop
+
+
+                End If
+                If totaldealercard > 21 Then
+                    gameover = 1
+                    MessageBox.Show("you win")
+                ElseIf totaldealercard <= 21 Then
+                    If totalplayercard > totaldealercard Then
+                        gameover = 1
+                        MessageBox.Show("you win")
+                    Else
+                        gameover = 1
+                        MessageBox.Show("youlose ")
+
+                    End If
+                End If
+
+            End If
+        End If
+    End Sub
+
+    Private Sub reset_Click(sender As Object, e As EventArgs) Handles reset.Click
+        PulledCrardIndex = 0
+        deck = New String(51) {}
+        ShuffledDeck = New String(51) {}
+        ShuffledDeckValues = New Integer(51) {}
+
+        player1 = 0
+        player2 = 0
+        player3 = 0
+        player4 = 0
+        player5 = 0
+        player6 = 0
+
+        dealer1 = 0
+        dealer2 = 0
+        dealer3 = 0
+        dealer4 = 0
+        dealer5 = 0
+        dealer6 = 0
+
+
+        totaldealercard = 0
+
+        totalplayercard = 0
+
+        loopcheck = 0
+        startcheck = 0
+        hitcheck = 0
+        standcheck = 0
+        gameover = 0
+
+        p1.Text = 0
+        p2.Text = 0
+        p3.Text = 0
+        p4.Text = 0
+        p5.Text = 0
+        p6.Text = 0
+
+        p1Card.Text = ""
+        p2Card.Text = ""
+        p3Card.Text = ""
+        p4Card.Text = ""
+        p5Card.Text = ""
+        p6Card.Text = ""
+
+        d1.Text = 0
+        d2.Text = 0
+        d3.Text = 0
+        d4.Text = 0
+        d5.Text = 0
+        d6.Text = 0
+
+        d1Card.Text = ""
+        d2Card.Text = ""
+        d3Card.Text = ""
+        d4Card.Text = ""
+        d5Card.Text = ""
+        d6Card.Text = ""
+
+        pTotal.Text = 0
+        dTotal.Text = 0
+
+        d2CardName = ""
+
+
+    End Sub
+
+    Function pullcard() As Double
+        CurrentCardValue = ShuffledDeckValues(PulledCrardIndex)
+        CurrentCardName = ShuffledDeck(PulledCrardIndex)
+        PulledCrardIndex += 1
+        Return CurrentCardValue
+    End Function
+
+    Function create_a_deck() As String()
+        deck = New String(51) {}
+        Dim i As Integer = 0
+        For i = 0 To 51
+            deck(i) = Masterdeck(i)
+        Next i
+        Return deck
+    End Function
+
+    Function shuffle_deck() As String()
+        ShuffledDeck = New String(51) {}
+        ShuffledDeckValues = New Integer(51) {}
+        Dim i As Integer = 0
+        Dim R As New Random
+        Dim card As String = "X"
+        Dim index As Integer = 0
+
+        For i = 0 To 51
+            index = R.Next(0, deck.Length)
+            card = deck(index)
+            If card = "X" Then
+                Do
+                    index += 1
+                    If index > 51 Then
+                        index = 0
+                    End If
+                Loop While deck(index) = "X"
+
+                card = deck(index)
+            End If
+
+            ShuffledDeck(i) = card
+            ShuffledDeckValues(i) = get_card_value(card)
+
+            deck(index) = "X"
+        Next i
+
+        Return ShuffledDeck
+    End Function
+
+    Function get_card_value(card_name As String) As Integer
+        If card_name = "2c" Or card_name = "2h" Or card_name = "2s" Or card_name = "2d" Then
+            Return 2
+        ElseIf card_name = "3c" Or card_name = "3h" Or card_name = "3s" Or card_name = "3d" Then
+            Return 3
+        ElseIf card_name = "4c" Or card_name = "4h" Or card_name = "4s" Or card_name = "4d" Then
+            Return 4
+        ElseIf card_name = "5c" Or card_name = "5h" Or card_name = "5s" Or card_name = "5d" Then
+            Return 5
+        ElseIf card_name = "6c" Or card_name = "6h" Or card_name = "6s" Or card_name = "6d" Then
+            Return 6
+        ElseIf card_name = "7c" Or card_name = "7h" Or card_name = "7s" Or card_name = "7d" Then
+            Return 7
+        ElseIf card_name = "8c" Or card_name = "8h" Or card_name = "8s" Or card_name = "8d" Then
+            Return 8
+        ElseIf card_name = "9c" Or card_name = "9h" Or card_name = "9s" Or card_name = "9d" Then
+            Return 9
+        ElseIf card_name = "10c" Or card_name = "10h" Or card_name = "10s" Or card_name = "10d" Then
+            Return 10
+        ElseIf card_name = "Jc" Or card_name = "Jh" Or card_name = "Js" Or card_name = "Jd" Then
+            Return 10
+        ElseIf card_name = "Qc" Or card_name = "Qh" Or card_name = "Qs" Or card_name = "Qd" Then
+            Return 10
+        ElseIf card_name = "Kc" Or card_name = "Kh" Or card_name = "Ks" Or card_name = "Kd" Then
+            Return 10
+        ElseIf card_name = "Ac" Or card_name = "Ah" Or card_name = "As" Or card_name = "Ad" Then
+            Return 11
+        End If
+        Return 0
+    End Function
+End Class
